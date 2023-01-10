db6a7d631b9b59cd10f500ebff6e4053d17a185d
Swiftor lilko <swifrorlilko@gmail.com>
2023-01-10 09:04:43 +0200

    FIX: overflow when clac expire time, thread bug when calling self delete

--- src/server/store.rs
+++ src/server/store.rs
@@ -1,10 +1,12 @@
+use std::num::Wrapping;
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 pub struct Store {
-    data: Arc<Mutex<HashMap<String, String>>>,
+    data: Arc<Mutex<HashMap<String, (String, u64)>>>,
 }
 
 impl Store {
@@ -14,22 +16,67 @@ impl Store {
         }
     }
 
-    pub fn insert(&self, key: String, value: String) {
+    pub fn insert(&self, key: String, value: String, seconds: u64) {
         let mut data = self.data.lock().unwrap();
-        data.insert(key, value);
+
+        if seconds == 0 {
+            data.insert(key, (value, 0));
+            return;
+        }
+
+        let expire_in = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            + seconds;
+
+        data.insert(key, (value, expire_in));
     }
 
     pub fn get(&self, key: &str) -> Option<String> {
-        let data = self.data.lock().unwrap();
-        match data.get(key) {
-            Some(value) => Some(value.to_owned()),
-            None => None,
+        let mut data = self.data.lock().unwrap();
+
+        let value = data.get(key).is_none();
+
+        if value {
+            return None;
         }
+
+        let value = data.get(key).unwrap();
+
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+            
+        if value.1 == 0 {
+            return Some(value.0.clone());
+        }
+
+        if now > value.1 {
+            data.remove(key);
+            return None;
+        }
+
+        return Some(value.0.clone());
     }
-    
 
     pub fn delete(&self, key: &str) {
         let mut data = self.data.lock().unwrap();
         data.remove(key);
+    }
+
+    pub fn expires_in(&self, key: &str) -> Option<u64> {
+        let data = self.data.lock().unwrap();
+
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        match data.get(key) {
+            Some(value) => Some((Wrapping(value.1) - Wrapping(now)).0), // If is expired and is 0 will not overflow and return large number
+            None => None,
+        }
     }
 }
