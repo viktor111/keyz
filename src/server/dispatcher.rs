0f75df9c60b3139a82c5d51fe641934f48cc1a94
Swiftor lilko <swifrorlilko@gmail.com>
2023-01-10 09:03:13 +0200

    ADD: set command parser using REGEX

--- src/server/dispatcher.rs
+++ src/server/dispatcher.rs
@@ -1,10 +1,16 @@
 use std::error::Error;
 
-use super::{store::Store, commands::{get, delete, set}};
+use regex::Regex;
+
+use super::{
+    commands::{delete, expires_in, get, set},
+    store::Store,
+};
 
 const SET: &str = "SET";
 const GET: &str = "GET";
 const DELETE: &str = "DELETE";
+const EXPIRES_IN: &str = "EXIN";
 
 /// Dispatch a command to the store
 ///
@@ -19,24 +25,57 @@ const DELETE: &str = "DELETE";
 ///
 /// # Errors
 ///
-/// * If the command is invalid 
+/// * If the command is invalid
 pub async fn dispatcher(command: String, store: &mut Store) -> Result<String, Box<dyn Error>> {
     let splited: Vec<&str> = command.splitn(3, ' ').collect();
 
     let command_name = splited[0];
-    let key = splited[1];
 
-    let mut value = "";
-    
-    if splited.len() > 2 {
-        value = splited[2];
-    }
-    
+    let key = splited[1].to_string();
+
+    // SET user:1 {"name": "John", "age": 30} EX 10
 
     match command_name {
-        SET => set(key, value, store),
-        GET => get(key, store),
-        DELETE => delete(key, store),
+        SET => {
+            let (key, value, seconds) = parse_set_command(&command);
+            set(&key, value, store, seconds)
+        }
+        GET => get(&key, store),
+        DELETE => delete(&key, store),
+        EXPIRES_IN => expires_in(&key, store),
         _ => Ok("[!] Invalid command".to_string()),
+    }
+}
+
+fn parse_set_command(input: &str) -> (String, String, u64) {
+    let re = Regex::new(r"SET\s+(\S+)\s+(.+?)(?:\s+EX)(\s+\d+)$").unwrap();
+
+    // Match on the captures and return the appropriate tuple
+    match re.captures(input) {
+        Some(captures) => {
+            // Get the key and value from the captures
+            let key = captures[1].to_string();
+            let value = captures[2].to_string();
+
+            // Get the expire time, if it was provided
+            let expire = captures.get(3).is_some();
+
+            if expire {
+                (key, value, captures[3].trim().parse::<u64>().unwrap())
+            }
+            else {
+                (key, value, 0)
+            }
+        }
+        None => {
+            let re = Regex::new(r"SET\s+(\S+)\s+(.+)(?:\s+EX\s+(\d+))?").unwrap();
+            let captures = re.captures(input).unwrap();
+
+            // Get the key and value from the captures
+            let key = captures[1].to_string();
+            let value = captures[2].to_string();
+
+            (key, value, 0)
+        }
     }
 }
